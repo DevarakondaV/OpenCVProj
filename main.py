fe133ec7590731e9aab936fcf8d086f769730af5
DevaraV <Devarakonda.vishnu5@gmail.com>
2017-09-01 12:03:59 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import numpy as np
 import cv2;
 
 cap  = cv2.VideoCapture(0)
-fgbg = cv2.createBackgroundSubtractorMOG2();
+fgbg = cv2.createBackgroundSubtractorMOG2()
 Thres = 0;#150,200
 Upper = 0;
 Type = 0;
-
+YellowLower = [0,160,160]
+YellowUpper = [135,255,255]
 
 
 
@@ -33,19 +34,35 @@ cv2.createTrackbar("Type","Frame",Type,5,TypeChange)
 
 
 def fun():
-    lower = np.array([0,10,60],dtype="uint8")
-    upper = np.array([20,150,255],dtype="uint8")
+    lower = np.array(YellowLower,dtype="uint8")
+    upper = np.array(YellowUpper,dtype="uint8")
 
     while(1):
         ret,frame = cap.read()
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Clean",frame)
+        #Reducing Image Blur
+        MedBlur = cv2.medianBlur(frame,3)
+        GusBlur = cv2.GaussianBlur(MedBlur,(3,3),2)
+
+        #Selecting Color Range
+        gray = cv2.inRange(GusBlur,lower,upper)
+        cv2.imshow("Gray",gray)
+
+        #Morphology Transofrmations
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
+        erode = cv2.erode(gray,kernel,iterations=5)
+
+        #Finding Contours
+        im,con,heir = cv2.findContours(erode,cv2.RETR_TREE,cv2.CHAIN_APPROX_TC89_KCOS)
+        cv2.drawContours(frame,con,-1,(0,255,0),3)
+        cv2.imshow("ConView",frame)
+        '''
         gray = cv2.GaussianBlur(gray,(5,5),10)
-        #rep,thresh = cv2.threshold(gray,Thres,Upper,Type)
-        #thresh = cv2.adaptiveThreshold(gray,Thres,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,5,4)
-        rep, thresh = cv2.threshold(gray,153,140,cv2.THRESH_BINARY_INV)
+        rep , thresh = cv2.threshold(gray,Thres,Upper,Type)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
         thresh = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel)
 
+        
         im2,con,hier = cv2.findContours(thresh,2,1)
         #cv2.drawContours(frame,con,-1,(0,255,0),3)
         cnt = con[2]
@@ -58,10 +75,10 @@ def fun():
                 end = tuple(cnt[e][0])
                 far = tuple(cnt[f][0])
                 cv2.line(frame,start,end,(0,255,0),2)
-                cv2.circle(frame,far,5,(0,0,255),-1)
+                cv2.circle(frame,far,5,(0,0,255),-1)'''
 
-        cv2.imshow('Frame',thresh)
-        cv2.imshow('frame',frame)
+
+
         k = cv2.waitKey(30) & 0xff
         if k == 27:
             break;
